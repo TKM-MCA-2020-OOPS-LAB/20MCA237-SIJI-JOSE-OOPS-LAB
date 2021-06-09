7cd01b342a4c0704c60f689755c472226c164b61
siji1999 <sijijose.sj1999@gmail.com>
2021-06-09 12:43:19 +0530

    lab

---
+++ co4/co4-q3/user.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class user
+{
+    public static void main(String args[])
+    {
+        String username, password;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username:");
+        username = sc.nextLine();
+        System.out.print("Enter password:");
+        password = sc.nextLine();
+        try
+        {
+            if(username.equals("admin") && password.equals("hello123"))
+            {
+            System.out.println("Authentication Successful");
+           }
+           else
+           {
+            System.out.println("Authentication Failed");
+           }
+       }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+
+        }
+}
+}
