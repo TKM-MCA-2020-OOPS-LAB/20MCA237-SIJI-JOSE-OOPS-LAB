19da25983ef361d96865e07b2a4df9e19a815663
siji1999 <sijijose.sj1999@gmail.com>
2021-08-06 21:16:51 +0530

    lab

---
+++ co4/co4-q12/stack.java
@@ -0,0 +1,34 @@
+// Java code to illustrate remove() when position of
+// element is passed as parameter
+
+import java.util.*;
+
+public class stack{
+	public static void main(String args[])
+	{
+		// Creating an empty Stack
+		Stack<String> stack = new Stack<String>();
+
+		// Use add() method to add elements in the Stack
+		stack.add("megha");
+		stack.add("diya");
+		stack.add("varun");
+		stack.add("nila");
+		stack.add("veena");
+
+		// Output the Stack
+		System.out.println("Stack: " + stack);
+
+		// Remove the element using remove()
+		String rem = stack.remove(1);
+
+		// Print the removed element
+		System.out.println("Removed element: "
+						+ rem);
+
+		// Print the final Stack
+		System.out.println("Final Stack: "
+						+ stack);
+	}
+}
+
